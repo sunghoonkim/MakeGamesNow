9dd92daac959f106e60260b46ed3902b06feff79
sunghoonkim <sunghoonkim@Sungs-MacBook-Pro.local>
2015-09-26 16:59:24 -0400

    Enemy's explode

---
+++ Assets/Scripts/GibOnTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GibOnTrigger : MonoBehaviour {
+
+	public GameObject gib;
+
+
+	void OnTriggerEnter(){
+
+		Instantiate (gib, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+	}
+
+
+
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+}
