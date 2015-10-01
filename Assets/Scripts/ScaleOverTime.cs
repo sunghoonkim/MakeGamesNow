92207af6cea49f10ca2c608b1325a933597061e7
sunghoonkim <sunghoonkim@172-16-224-227.DYNAPOOL.NYU.EDU>
2015-10-01 18:53:42 -0400

    moving objects

--- Assets/Scripts/ScaleOverTime.cs
+++ Assets/Scripts/ScaleOverTime.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class ScaleOverTime : MonoBehaviour {
 
+	public Vector3 finalScale=Vector3.zero;
+	private Vector3 initialScale;
+	public float time=1.0f;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		initialScale = transform.localScale;
+		StartCoroutine ("Scale");
 	}
+
+
 	
-	// Update is called once per frame
-	void Update () {
-	
+	IEnumerator Scale(){
+
+		float t = 0.0f;
+
+		while (t <= time) {
+
+			transform.position=Vector3.Lerp(initialScale, finalScale,t/time);
+			t+=Time.deltaTime;
+
+			yield return null;
+
+		}
+
+		transform.position = finalScale;
 	}
 }
